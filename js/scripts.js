70e478b2666679900511a908258b505ecd1fa092
cm <connormccarthy8@gmail.com>
2018-08-10 09:12:01 -0700

    add js var

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $("document").ready(function(){
-  
+  var learn = parseInt($("input:radio[name=learn]:checked").val());
+  var size = parseInt($("input:radio[name=size]:checked").val());
+  var frontBackEnd = parseInt($("input:radio[name=front-back]:checked").val());
+  var app = parseInt($("input:radio[name=app]:checked").val());
+  var location = parseInt($("input:radio[name=location]:checked").val());
+
+  var quizScore = learn + size + frontBackEnd + app + location
 });
