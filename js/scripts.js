b55be0d9331e5465084b6a3e1d1c7b211389f390
cm <connormccarthy8@gmail.com>
2018-08-10 09:31:02 -0700

    add js to show all results when questions are selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 $("document").ready(function(){
-  var learn = parseInt($("input:radio[name=learn]:checked").val());
-  var size = parseInt($("input:radio[name=size]:checked").val());
-  var frontBackEnd = parseInt($("input:radio[name=front-back]:checked").val());
-  var app = parseInt($("input:radio[name=app]:checked").val());
-  var location = parseInt($("input:radio[name=location]:checked").val());
+  $("form#questions").submit(function(event){
+    var learn = parseInt($("input:radio[name=learn]:checked").val());
+    var size = parseInt($("input:radio[name=size]:checked").val());
+    var frontBackEnd = parseInt($("input:radio[name=front-back]:checked").val());
+    var app = parseInt($("input:radio[name=app]:checked").val());
+    var location = parseInt($("input:radio[name=location]:checked").val());
 
-  var quizScore = learn + size + frontBackEnd + app + location
+    var quizScore = learn + size + frontBackEnd + app + location
+
+    if (quizScore === 5)  {
+      $("#results").show();
+    }
+
+    event.preventDefault();
+  });
 });
